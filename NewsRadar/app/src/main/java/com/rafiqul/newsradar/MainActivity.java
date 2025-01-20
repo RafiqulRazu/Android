3ad1e792984b99cd012da83fa9306e4101161ef1
RafiqulRazu <158472447+RafiqulRazu@users.noreply.github.com>
2025-01-21 05:10:23 +0600

    add
    daily news app by android

---
+++ NewsRadar/app/src/main/java/com/rafiqul/newsradar/MainActivity.java
@@ -0,0 +1,143 @@
+package com.rafiqul.newsradar;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.progressindicator.LinearProgressIndicator;
+import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.Article;
+import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    RecyclerView recyclerView;
+    List<Article> articleList = new ArrayList<>();
+    NewsRecyclerAdapter adapter;
+    LinearProgressIndicator progressIndicator;
+
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7;
+
+    SearchView searchView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.news_recycler_view);
+        progressIndicator = findViewById(R.id.progress_bar);
+
+        setupRecyclerView();
+        getNews("GENERAL", null);
+
+        btn1 = findViewById(R.id.btn_1);
+        btn1.setOnClickListener(this);
+
+        btn2 = findViewById(R.id.btn_2);
+        btn2.setOnClickListener(this);
+
+        btn3 = findViewById(R.id.btn_3);
+        btn3.setOnClickListener(this);
+
+        btn4 = findViewById(R.id.btn_4);
+        btn4.setOnClickListener(this);
+
+        btn5 = findViewById(R.id.btn_5);
+        btn5.setOnClickListener(this);
+
+        btn6 = findViewById(R.id.btn_6);
+        btn6.setOnClickListener(this);
+
+        btn7 = findViewById(R.id.btn_7);
+        btn7.setOnClickListener(this);
+
+        searchView = findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                getNews("GENERAL", query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+
+    }
+
+    void setupRecyclerView(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new NewsRecyclerAdapter(articleList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    void changeInProgress(boolean show){
+        if(show)
+            progressIndicator.setVisibility(recyclerView.VISIBLE);
+        else
+            progressIndicator.setVisibility(recyclerView.INVISIBLE);
+    }
+
+    void getNews(String category, String query){
+        changeInProgress(true);
+        NewsApiClient newsApiClient = new NewsApiClient("de3e3287973a4a0dae43fdd2ac540054");
+        newsApiClient.getTopHeadlines(
+                new TopHeadlinesRequest.Builder()
+                        .language("en")
+                        .category(category)
+                        .q(query)
+                        .build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+//                        response.getArticles().forEach((a)->{
+//                            Log.i("Article",a.getTitle());
+//                        });
+
+                        runOnUiThread(()->{
+                            changeInProgress(false);
+                            articleList = response.getArticles();
+                            adapter.updateData(articleList);
+                            adapter.notifyDataSetChanged();
+                        });
+
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.i(throwable.getMessage(), "GOT FAILURE");
+                    }
+                }
+
+        );
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Button btn = (Button) v;
+        String category = btn.getText().toString();
+        getNews(category, null);
+
+    }
+}
